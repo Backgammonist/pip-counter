7d1f24fee8ee93cc2460a27aa1b5250c66d6abba
Laszlo Heves <laszlo_heves@discovery.com>
2017-08-24 13:10:11 +0100

    chore(*): add eslint

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,10 @@
+/* eslint no-console: "off" */
+
 const count = require('./count');
 
-let mainState = count.shuffle()
+let mainState = count.shuffle();
 
-console.log(mainState)
+console.log(mainState);
 
-console.log(count.printPip('player', mainState))
-console.log(count.printPip('opponent', mainState))
+console.log(count.printPip('player', mainState));
+console.log(count.printPip('opponent', mainState));
