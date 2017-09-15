969267fb4dac6e8bc688a0a5dfcbc1786958a46b
Laszlo Heves <laszlo_heves@discovery.com>
2017-09-15 17:06:42 +0100

    chore(components): add propType validation

--- src/components/Field.js
+++ src/components/Field.js
@@ -1,3 +1,5 @@
+import PropTypes from 'prop-types';
+
 import React, { PureComponent } from 'react';
 
 import Checker from './checkers/Checker';
@@ -7,10 +9,12 @@ class Field extends PureComponent {
     render() {
         const {
             ordinal,
+            player,
+            opponent
         } = this.props;
 
         let checkers = [];
-        if (this.props.player || this.props.opponent) {
+        if (player || opponent) {
             const playerType = this.props.player ? 'player' : 'opponent';
             for (var index = 0; index <this.props[playerType];  index++ ) {
                 checkers.push(<Checker key={index} playerType={playerType} />);
@@ -27,5 +31,11 @@ class Field extends PureComponent {
         );
     }
 }
+
+Field.propTypes = {
+    ordinal: PropTypes.number,
+    player: PropTypes.object,
+    opponent: PropTypes.object
+};
 
 export default Field;
--- src/components/checkers/Checker.js
+++ src/components/checkers/Checker.js
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types';
 import React, { PureComponent } from 'react';
 import './checker.css';
 
@@ -5,8 +6,12 @@ class Checker extends PureComponent {
     render () {
         return (
             <div className={`checker checker-${this.props.playerType === 'player' ? 'white': 'black'}`}></div>
-        )
+        );
     }
 }
+
+Checker.propTypes = {
+    playerType: PropTypes.string
+};
 
 export default  Checker;
