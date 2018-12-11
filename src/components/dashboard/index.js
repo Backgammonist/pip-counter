e13615e2934cf440cd7b76e2faf62bd2beea0e09
Laszlo Heves <laszloheves@admins-MacBook-Pro.local>
2018-12-11 10:11:00 +0000

    feat(scores): display pip diff

--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -10,16 +10,25 @@ import './dashboard.scss';
 
 class Dashboard extends PureComponent {
     render() {
+        const opponentPip = countPip('opponent', this.props.initialState)
+        const playerPip = countPip('player', this.props.initialState)
+        const diffPip = playerPip - opponentPip
+
         return (
             <div className="dashboard">
                 <div className="dashboard__scores">
                     <div className="dashboard__score">
                         <Checker key="opponent-checker" playerType="opponent" />
-                        <strong>{countPip('opponent', this.props.initialState)}</strong>
+                        <strong>{opponentPip}</strong>
+                        <div className="dashboard__scores--percent">({Math.round(opponentPip/ (opponentPip + playerPip) * 100)}%)</div>
+                    </div>
+                    <div className={`dashboard__diff ${diffPip > 0 ? 'green': 'red'}`}>
+                        {diffPip > 0 ? `+${diffPip}` : diffPip}
                     </div>
                     <div className="dashboard__score">
                         <Checker key="player-checker" playerType="player" />
-                        <strong>{countPip('player', this.props.initialState)}</strong>
+                        <strong>{playerPip}</strong>
+                        <div className="dashboard__scores--percent">({Math.round(playerPip/ (opponentPip + playerPip) * 100)}%)</div>
                     </div>
                 </div>
                 <div>
