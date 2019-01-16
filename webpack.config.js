ebc2afefbd51825af5441bd02fa451cac737636e
Laszlo Heves <heves.laszlo@gmail.com>
2019-01-16 17:06:15 +0000

    feat(*): update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,34 @@
+const webpack = require('webpack');
+
 module.exports = {
-  mode: "development",
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
-      },
-      {
-          test: /\.css$/,
-          use: [ 'style-loader', 'css-loader' ]
-      },
-      {
-          test: /\.scss$/,
-          use: [ 'style-loader', 'css-loader', 'sass-loader' ]
-      }
-    ]
-  }
+	mode: 'development',
+	entry: {
+		app: './src/index.js'
+	},
+	devtool: 'inline-source-map',
+	plugins: [ new webpack.HotModuleReplacementPlugin() ],
+	devServer: {
+		contentBase: './dist',
+		hot: true,
+		port: 9000
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader'
+				}
+			},
+			{
+				test: /\.css$/,
+				use: [ 'style-loader', 'css-loader' ]
+			},
+			{
+				test: /\.scss$/,
+				use: [ 'style-loader', 'css-loader', 'sass-loader' ]
+			}
+		]
+	}
 };
